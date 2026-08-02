cb0db198d5dca238e23c63c293861b2799f367cc
Harsh Rajbhar <harshrajbhar071@gmail.com>
2026-08-02 21:46:09 +0530

    Made Some Changes in ui.py

--- src/ui.py
+++ src/ui.py
@@ -58,38 +58,67 @@ HEIGHT = 720
 
 class MainUi(object):
     def setup(self, MainWindow):
-        if not MainWindow.objectName():
-            MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(WIDTH, HEIGHT)
-
-        self.centralWidget = QWidget()
-        MainWindow.setCentralWidget(self.centralWidget)
-
+        self.MainWindow = MainWindow
+        self.setupWindow()
         # Main Layout
-        self.mainLayout = QVBoxLayout(self.centralWidget)
+        self.setupMainLayout()
 
-        # Navigation Bar
-        self.navFrame = QFrame()
-        self.navFrame.setStyleSheet("""background-color: #f58442;
-                                       border-radius: 12px;
-                                       """)
-        self.mainLayout.addWidget(self.navFrame, 1)
+        # Navigation Bar 
+        self.setupNavigationBar()
 
         # Media Layout
+        self.setupMediaLayout()
+
+        # Control Bar 
+        self.setupControlsBar()
+        
+    def setupWindow(self):
+        if not self.MainWindow.objectName():
+            self.MainWindow.setObjectName("MainWindow")
+        self.MainWindow.resize(WIDTH, HEIGHT)
+            
+    def setupMainLayout(self):
+        self.centralWidget = QWidget()
+        self.centralWidget.setStyleSheet("background-color: #121212")
+        self.MainWindow.setCentralWidget(self.centralWidget)
+        self.mainLayout = QVBoxLayout(self.centralWidget)
+    
+    def setupNavigationBar(self):
+        # Navigation Bar #1E1E1E
+        self.navFrame = QFrame()
+        self.navFrame.setStyleSheet("""background-color: #C9C9C9;
+                                    border-radius: 12px;""")
+        self.mainLayout.addWidget(self.navFrame, 1)
+        
+    def setupMediaLayout(self):
+         # Media Layout #181818
         self.mediaFrame = QFrame()
-        self.mediaFrame.setStyleSheet("""
-                                      background-color: #3b3b3b;
-                                      border-radius: 12px;
-                                      """)
+        self.mediaFrame.setStyleSheet("""background-color: #8F8F8F;
+                                      border-radius: 12px;""")
+        
+        self.mediaLayout = QHBoxLayout(self.mediaFrame)
+        # Media Player Frame #282828
+        self.playerFrame = QFrame()
+        self.playerFrame.setStyleSheet("""background-color: #E6E6E6;
+                                       border-radius: 12px;""")
+        
+        # Media Playlist Frame #282828
+        self.playlistFrame = QFrame()
+        self.playlistFrame.setStyleSheet("""background-color: #E6E6E6;
+                                         border-radius: 12px;""")
+        
+        self.mediaLayout.addWidget(self.playerFrame, 4)
+        self.mediaLayout.addWidget(self.playlistFrame, 1)
         self.mainLayout.addWidget(self.mediaFrame, 8)
-
-        # Control Bar
+    
+    def setupControlsBar(self):
+        # Control Bar #1E1E1E
         self.controlsFrame = QFrame()
-        self.controlsFrame.setStyleSheet("""background-color: #f58442;
-                                       border-radius: 12px;
-                                       """)
+        self.controlsFrame.setStyleSheet("""background-color: #C9C9C9;
+                                         border-radius: 12px;
+                                         """)
         self.mainLayout.addWidget(self.controlsFrame, 2)
-
+        
     def addLabel(self, Text):
         label = QLabel(Text)
         label.setAlignment(Qt.AlignmentFlag.AlignCenter)
